6285dd1341aa8fecc4e4450bd53c740b3564c075
Naoki Kinoshita <uttnaoki@gmail.com>
2017-12-05 10:13:06 +0900

    地域選択後のズーム処理を追加

--- js/index.js
+++ js/index.js
@@ -28,32 +28,72 @@ const DEF_map_style = {
 }
 
 function setRegionStyle(r) {
-  let map_style = DEF_map_style;
-  let s,f;
+  let region_style = {};
+  let s, f;
   switch (r) {
     case '北海道':
       s=1; f=1;
+      region_style = {
+        "lat": 43.568246,
+        "lng": 142.740234,
+        "zoom_level": 8
+      };
       break;
     case '東北':
       s=2; f=7;
+      region_style = {
+        "lat": 39.338327,
+        "lng": 140.916504,
+        "zoom_level": 8
+      };
       break;
     case '関東':
       s=8; f=14;
+      region_style = {
+        "lat": 35.764113,
+        "lng": 139.823364,
+        "zoom_level": 9
+      };
       break;
     case '中部':
       s=15; f=23;
+      region_style = {
+        "lat": 35.968885,
+        "lng": 137.373413,
+        "zoom_level": 8
+      };
       break;
     case '近畿':
       s=24; f=30;
+      region_style = {
+        "lat": 34.903720,
+        "lng": 135.654053,
+        "zoom_level": 9
+      };
       break;
     case '中国':
       s=31; f=35;
+      region_style = {
+        "lat": 34.822590,
+        "lng": 132.825073,
+        "zoom_level": 9
+      };
       break;
     case '四国':
-      s=36; f=39
+      s=36; f=39;
+      region_style = {
+        "lat": 33.586931,
+        "lng": 133.495239,
+        "zoom_level": 9
+      };
       break;
     case '九州':
       s=40; f=47;
+      region_style = {
+        "lat": 30.867987,
+        "lng": 130.787109,
+        "zoom_level": 7
+      };
       break;
     default:
   }
@@ -64,17 +104,21 @@ function setRegionStyle(r) {
     const key = ("0" + i).slice(-2);
     area_color[key] = region_color.area;
   }
+  let map_style = DEF_map_style;
   map_style.area = area_color;
+  region_style.map = map_style;
 
-  return map_style;
+  return region_style;
 }
 
 function goToRegion(button) {
   const r = button.value;
-  const map_style = setRegionStyle(r);
+  const region_style = setRegionStyle(r);
 
-  blankmap.setStyle(map_style)
+  blankmap.setStyle(region_style.map)
   map.setLayerSet("blankmap")
+  const p = new Y.LatLng(region_style.lat, region_style.lng);
+  map.setZoom(region_style.zoom_level, true, p, true);
 }
 
 window.onload = function() {
