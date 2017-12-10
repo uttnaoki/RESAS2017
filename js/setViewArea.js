29c5effdc1012fe5a425c9734dd6a16ff7f7d615
Naoki Kinoshita <uttnaoki@gmail.com>
2017-12-10 20:08:35 +0900

    setViewArea()を別ファイルで定義し，都道府県にも対応

---
+++ js/setViewArea.js
@@ -0,0 +1,394 @@
+function setViewArea(r) {
+  switch (r) {
+    case '全国':
+      view_area = {
+        "lat": 37,
+        "lng": 135.2,
+        "zoom_level": 6
+      }
+      break;
+    case '北海道':
+      view_area = {
+        "lat": 43.568246,
+        "lng": 142.740234,
+        "zoom_level": 8
+      };
+      break;
+    case '東北':
+      view_area = {
+        "lat": 39.338327,
+        "lng": 140.916504,
+        "zoom_level": 8
+      };
+      break;
+    case '関東':
+      view_area = {
+        "lat": 35.764113,
+        "lng": 139.823364,
+        "zoom_level": 9
+      };
+      break;
+    case '中部':
+      view_area = {
+        "lat": 35.968885,
+        "lng": 137.373413,
+        "zoom_level": 8
+      };
+      break;
+    case '近畿':
+      view_area = {
+        "lat": 34.903720,
+        "lng": 135.654053,
+        "zoom_level": 9
+      };
+      break;
+    case '中国':
+      view_area = {
+        "lat": 34.822590,
+        "lng": 132.825073,
+        "zoom_level": 9
+      };
+      break;
+    case '四国':
+      view_area = {
+        "lat": 33.586931,
+        "lng": 133.495239,
+        "zoom_level": 9
+      };
+      break;
+    case '九州':
+      view_area = {
+        "lat": 30.867987,
+        "lng": 130.787109,
+        "zoom_level": 7
+      };
+      break;
+    case '沖縄':
+      view_area = {
+        "lat": 25.77111111,
+        "lng": 126.64,
+        "zoom_level": 8
+      };
+      break;
+
+
+    case '青森':
+      view_area = {
+        "lat": 40.88027778,
+        "lng": 140.8319444,
+        "zoom_level": 10
+      };
+      break;
+    case '岩手':
+      view_area = {
+        "lat": 39.59138889,
+        "lng": 141.3625,
+        "zoom_level": 9
+      };
+      break;
+    case '宮城':
+      view_area = {
+        "lat": 38.44555556,
+        "lng": 140.9280556,
+        "zoom_level": 10
+      };
+      break;
+    case '秋田':
+      view_area = {
+        "lat": 39.7475,
+        "lng": 140.4086111,
+        "zoom_level": 9
+      };
+      break;
+    case '山形':
+      view_area = {
+        "lat": 38.44638889,
+        "lng": 140.1027778,
+        "zoom_level": 10
+      };
+      break;
+    case '福島':
+      view_area = {
+        "lat": 37.37888889,
+        "lng": 140.2252778,
+        "zoom_level": 10
+      };
+      break;
+    case '茨城':
+      view_area = {
+        "lat": 36.30638889,
+        "lng": 140.3186111,
+        "zoom_level": 10
+      };
+      break;
+    case '栃木':
+      view_area = {
+        "lat": 36.68916667,
+        "lng": 139.8191667,
+        "zoom_level": 10
+      };
+      break;
+    case '群馬':
+      view_area = {
+        "lat": 36.50388889,
+        "lng": 138.9852778,
+        "zoom_level": 10
+      };
+      break;
+    case '埼玉':
+      view_area = {
+        "lat": 35.99666667,
+        "lng": 139.3477778,
+        "zoom_level": 11
+      };
+      break;
+    case '千葉':
+      view_area = {
+        "lat": 35.51277778,
+        "lng": 140.2038889,
+        "zoom_level": 10
+      };
+      break;
+    case '東京':
+      view_area = {
+        "lat": 35.71833333,
+        "lng": 139.3986111,
+        "zoom_level": 11
+      };
+      break;
+    case '神奈川':
+      view_area = {
+        "lat": 35.41416667,
+        "lng": 139.3402778,
+        "zoom_level": 11
+      };
+      break;
+    case '新潟':
+      view_area = {
+        "lat": 37.51888889,
+        "lng": 138.9172222,
+        "zoom_level": 9
+      };
+      break;
+    case '山梨':
+      view_area = {
+        "lat": 35.61222222,
+        "lng": 138.6116667,
+        "zoom_level": 10
+      };
+      break;
+    case '長野':
+      view_area = {
+        "lat": 36.13,
+        "lng": 138.0438889,
+        "zoom_level": 9
+      };
+      break;
+    case '富山':
+      view_area = {
+        "lat": 36.63611111,
+        "lng": 137.2680556,
+        "zoom_level": 11
+      };
+      break;
+    case '石川':
+      view_area = {
+        "lat": 36.81583333,
+        "lng": 136.7713889,
+        "zoom_level": 10
+      };
+      break;
+    case '福井':
+      view_area = {
+        "lat": 35.61222222,
+        "lng": 136.2272222,
+        "zoom_level": 10
+      };
+      break;
+    case '岐阜':
+      view_area = {
+        "lat": 35.7775,
+        "lng": 137.055,
+        "zoom_level": 10
+      };
+      break;
+    case '静岡':
+      view_area = {
+        "lat": 35.01694444,
+        "lng": 138.33,
+        "zoom_level": 10
+      };
+      break;
+    case '愛知':
+      view_area = {
+        "lat": 35.03444444,
+        "lng": 137.215,
+        "zoom_level": 10
+      };
+      break;
+    case '三重':
+      view_area = {
+        "lat": 34.49361111,
+        "lng": 136.3813889,
+        "zoom_level": 10
+      };
+      break;
+    case '滋賀':
+      view_area = {
+        "lat": 35.21527778,
+        "lng": 136.1380556,
+        "zoom_level": 10
+      };
+      break;
+    case '京都':
+      view_area = {
+        "lat": 35.25194444,
+        "lng": 135.4458333,
+        "zoom_level": 10
+      };
+      break;
+    case '大阪':
+      view_area = {
+        "lat": 34.62857778,
+        "lng": 135.5111111,
+        "zoom_level": 11
+      };
+      break;
+    case '兵庫':
+      view_area = {
+        "lat": 34.93094444,
+        "lng": 134.8286111,
+        "zoom_level": 10
+      };
+      break;
+    case '奈良':
+      view_area = {
+        "lat": 34.31555556,
+        "lng": 135.8713889,
+        "zoom_level": 10
+      };
+      break;
+    case '和歌山':
+      view_area = {
+        "lat": 33.90944444,
+        "lng": 135.5133333,
+        "zoom_level": 10
+      };
+      break;
+    case '鳥取':
+      view_area = {
+        "lat": 35.36055556,
+        "lng": 133.8516667,
+        "zoom_level": 11
+      };
+      break;
+    case '島根':
+      view_area = {
+        "lat": 35.07305556,
+        "lng": 132.5594444,
+        "zoom_level": 10
+      };
+      break;
+    case '岡山':
+      view_area = {
+        "lat": 34.90083333,
+        "lng": 133.8152778,
+        "zoom_level": 10
+      };
+      break;
+    case '広島':
+      view_area = {
+        "lat": 34.60361111,
+        "lng": 132.7875,
+        "zoom_level": 10
+      };
+      break;
+    case '山口':
+      view_area = {
+        "lat": 34.19861111,
+        "lng": 131.575,
+        "zoom_level": 10
+      };
+      break;
+    case '徳島':
+      view_area = {
+        "lat": 33.91805556,
+        "lng": 134.2430556,
+        "zoom_level": 11
+      };
+      break;
+    case '香川':
+      view_area = {
+        "lat": 34.24305556,
+        "lng": 133.9966667,
+        "zoom_level": 11
+      };
+      break;
+    case '愛媛':
+      view_area = {
+        "lat": 33.62194444,
+        "lng": 132.8558333,
+        "zoom_level": 10
+      };
+      break;
+    case '高知':
+      view_area = {
+        "lat": 33.42111111,
+        "lng": 133.3666667,
+        "zoom_level": 10
+      };
+      break;
+    case '福岡':
+      view_area = {
+        "lat": 33.5225,
+        "lng": 130.6680556,
+        "zoom_level": 10
+      };
+      break;
+    case '佐賀':
+      view_area = {
+        "lat": 33.28527778,
+        "lng": 130.1169444,
+        "zoom_level": 11
+      };
+      break;
+    case '長崎':
+      view_area = {
+        "lat": 33.2275,
+        "lng": 129.6141667,
+        "zoom_level": 10
+      };
+      break;
+    case '熊本':
+      view_area = {
+        "lat": 32.615,
+        "lng": 130.7563889,
+        "zoom_level": 10
+      };
+      break;
+    case '大分':
+      view_area = {
+        "lat": 33.19916667,
+        "lng": 131.4341667,
+        "zoom_level": 10
+      };
+      break;
+    case '宮崎':
+      view_area = {
+        "lat": 32.09083333,
+        "lng": 131.3005556,
+        "zoom_level": 10
+      };
+      break;
+    case '鹿児島':
+      view_area = {
+        "lat": 31.71277778,
+        "lng": 130.4241667,
+        "zoom_level": 10
+      };
+      break;
+    default:
+  }
+
+  return view_area;
+}
